3ed0c5411d9e8f991b67a19941dee1f5f1b3cb24
Aniket Tiwari <56513061+anikettiwari157@users.noreply.github.com>
2021-06-19 22:01:59 +0530

    initial commit

---
+++ curriculum/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from curriculum.models import *
+# Register your models here.
+
+
+admin.site.register(Standard)
+admin.site.register(Subject)
+admin.site.register(Lesson)
+admin.site.register(Comment)
+admin.site.register(Reply)
+admin.site.register(WorkingDay)
+admin.site.register(TimeSlot)
+admin.site.register(SlotSubject)
+admin.site.register(Event)
